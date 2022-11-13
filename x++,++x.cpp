89cdcc62bb3f80c4e9ba9bc8886a9eb3591db888
Mani sai Y <112387763+Manisai-459@users.noreply.github.com>
2022-11-13 22:28:38 +0530

    Create x++,++x.cpp

---
+++ x++,++x.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 0;
+    while (++x <= 5)
+        cout << "M";
+    cout << endl
+            x = 1;
+    while (x++ <= 5)
+        cout << "M";
+}
